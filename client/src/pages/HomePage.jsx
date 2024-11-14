1fcc618ab6cfa0ccf5dfcb21d0fb59400c156d7a
Radhi Hudijan <radhi.hudijan@accenture.com>
2024-11-14 16:56:22 +0100

    Refactor HomePage component and clean up App routes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
+/* eslint-disable */
 import React from "react";
 
-
-const HomePage=()=>{
-    return(
-        <div>
-            <h1>Home Page</h1>
-            <p>Hi, I am a full stack developer</p>
-        </div>
-    )
-}
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Hi, I am a full stack developer</p>
+    </div>
+  );
+};
 
 export default HomePage;
