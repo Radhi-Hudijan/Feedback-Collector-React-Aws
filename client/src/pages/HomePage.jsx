778526c921e6e434e2a2b95be51bbcaf7630bfee
Mohammed AL-ERYANI <mohammed.al-eryani@accenture.com>
2024-11-14 15:09:19 +0100

    Create frontend structure

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const HomePage=()=>{
+    return(
+        <div>
+            <h1>Home Page</h1>
+            <p>Hi, I am a full stack developer</p>
+        </div>
+    )
+}
+
+export default HomePage;
---
+++ client/src/pages/Sign.jsx
@@ -0,0 +1,9 @@
+const Sign = ({ sign, onClick }) => {
+  return (
+    <div>
+      <h1>Sign Page</h1>
+    </div>
+  );
+};
+
+export default Sign;
