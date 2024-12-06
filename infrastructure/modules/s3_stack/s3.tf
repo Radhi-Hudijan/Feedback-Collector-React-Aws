2e95c1a880e4d5e67a318e9f1eed70bfb0e70668
Mohammed AL-ERYANI <mohammed.al-eryani@accenture.com>
2024-12-06 15:34:37 +0100

    Add s3 acl and correct deploy react workflow

--- infrastructure/modules/s3_stack/s3.tf
+++ infrastructure/modules/s3_stack/s3.tf
@@ -2,3 +2,11 @@ resource "aws_s3_bucket" "react_hosting" {
   bucket        = "feadback-app-hosting-bucket"
   force_destroy = true
 }
+
+resource "aws_s3_bucket_acl" "react_hosting" {
+  bucket = aws_s3_bucket.react_hosting.id
+  acl    = "private"
+}
+
+
+# TODO: Add origin access control to allow cloudfront to access the bucket
