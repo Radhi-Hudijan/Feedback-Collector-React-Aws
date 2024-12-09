e1f1d778aeec652c988663b640c225456f94e1d6
Radhi Hudijan <radhi.hudijan@accenture.com>
2024-12-09 15:39:40 +0100

    Add output for client id in Cognito configuration
    Signed-off-by: Radhi Hudijan <radhi.hudijan@accenture.com>

--- infrastructure/modules/cognito/outputs.tf
+++ infrastructure/modules/cognito/outputs.tf
@@ -3,6 +3,7 @@ output "user_pool_id" {
   description = "value of the user pool id"
 }
 
+// output the client id
 output "client_id" {
   value       = aws_cognito_user_pool_client.feedbackClient.id
   description = "value of the client id"
