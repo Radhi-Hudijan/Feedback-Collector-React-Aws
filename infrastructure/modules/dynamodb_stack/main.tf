3318ee9dff09c770fa0433149eeb5ca1b5b2a150
Mohammed AL-ERYANI <mohammed.al-eryani@accenture.com>
2024-12-09 20:39:38 +0100

    Match the physical name with the resource name

--- infrastructure/modules/dynamodb_stack/main.tf
+++ infrastructure/modules/dynamodb_stack/main.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "feedback_table" {
-  name         = "feedback"
+  name         = "feedback_table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "feedback_id"
   range_key    = "user_id"
