18900e1a4193a786facfdcd6e802d69b3152d368
Radhi Hudijan <radhi.hudijan@accenture.com>
2024-12-09 15:28:09 +0100

    Add output for user pool ARN in Cognito configuration

--- infrastructure/modules/cognito/outputs.tf
+++ infrastructure/modules/cognito/outputs.tf
@@ -8,7 +8,7 @@ output "client_id" {
   description = "value of the client id"
 }
 
-
+// output the user pool arn
 output "user_pool_arn" {
   value       = aws_cognito_user_pool.feebackPool.arn
   description = "value of the user pool arn"
