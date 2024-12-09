17dca8019079cab61e8408007a4a2089c8809861
Mohammed AL-ERYANI <mohammed.al-eryani@accenture.com>
2024-12-09 13:59:28 +0100

    Add dynamodb table

---
+++ infrastructure/modules/dynamodb_stack/main.tf
@@ -0,0 +1,15 @@
+resource "aws_dynamodb_table" "feedback_table" {
+  name         = "feedback"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+  range_key    = "user_id"
+  #   deletion_protection_enabled = true
+  attribute {
+    name = "feedback_id"
+    type = "S"
+  }
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+}
