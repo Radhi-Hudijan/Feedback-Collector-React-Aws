debbd151700d8e6aa5643b6e8b5aa908fa5ba08a
Mohammed AL-ERYANI <mohammed.al-eryani@accenture.com>
2024-12-09 14:02:07 +0100

    Fix unmatched feedback id

--- infrastructure/modules/dynamodb_stack/main.tf
+++ infrastructure/modules/dynamodb_stack/main.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "feedback_table" {
   name         = "feedback"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
+  hash_key     = "feedback_id"
   range_key    = "user_id"
   #   deletion_protection_enabled = true
   attribute {
