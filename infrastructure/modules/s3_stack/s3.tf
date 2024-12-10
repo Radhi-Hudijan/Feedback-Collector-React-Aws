dd0105e8da1825b1d931af6de5f2ca921e4c5a94
Mohammed AL-ERYANI <mohammed.al-eryani@accenture.com>
2024-12-10 12:41:39 +0100

    Fix s3 acl issue

--- infrastructure/modules/s3_stack/s3.tf
+++ infrastructure/modules/s3_stack/s3.tf
@@ -3,10 +3,5 @@ resource "aws_s3_bucket" "react_hosting" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_acl" "react_hosting" {
-  bucket = aws_s3_bucket.react_hosting.id
-  acl    = "private"
-}
-
 
 # TODO: Add origin access control to allow cloudfront to access the bucket
