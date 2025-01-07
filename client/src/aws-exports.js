6890dec4d8beaaed5efa957b054621b4f963bcb5
Radhi Hudijan <radhi.hudijan@accenture.com>
2025-01-07 17:50:22 +0100

    Update AWS Cognito configuration and add package-lock.json

--- client/src/aws-exports.js
+++ client/src/aws-exports.js
@@ -1,8 +1,8 @@
 const awsmobile = {
   aws_project_region: "eu-central-1",
   aws_cognito_region: "eu-central-1",
-  aws_user_pools_id: "eu-central-1_AZ0E6IBOe",
-  aws_user_pools_web_client_id: "8ckehg57gg5ae60m1b2s70igc",
+  aws_user_pools_id: "eu-central-1_n4oUn08sf",
+  aws_user_pools_web_client_id: "34bi3ur6tputst7fssjbu2uj43",
 };
 
 export default awsmobile;
