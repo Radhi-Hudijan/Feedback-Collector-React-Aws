c4c5e7ed7cd084adee1085dbcfa550e1637d9993
Mohammed AL-ERYANI <mohammed.al-eryani@accenture.com>
2025-02-11 16:55:26 +0100

    refactor: consolidate IAM roles for feedback functions into a single role

--- infrastructure/modules/backend_functions_stack/iam.tf
+++ infrastructure/modules/backend_functions_stack/iam.tf
@@ -1,3 +1,19 @@
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "role_for_feedback_functions"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_iam_policy" "dynamodb_access" {
   name        = "dynamodb_access"
   description = "Allow access to dynamodb"
@@ -19,5 +35,16 @@ resource "aws_iam_policy" "dynamodb_access" {
     ],
   })
 }
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_access" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.dynamodb_access.arn
+}
+
 
 
